a6b283b43ebcb3143bec0b80bd4bc5f8e43f4c1f
mood7dev <mood7dev@gmail.com>
2025-08-07 12:20:41 +0900

    SignIn Serivce 완료

--- src/main/java/com/green/greengram/application/user/UserRepository.java
+++ src/main/java/com/green/greengram/application/user/UserRepository.java
@@ -3,4 +3,5 @@ import com.green.greengram.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUid(String uid);
 }
